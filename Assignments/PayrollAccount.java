bdd44801d1a98048105266ea223ebb888e58c963
AdarshRaghav23 <ar7087@srmist.edu.in>
2026-08-29 04:01:54 +0530

    Add files via upload

---
+++ Assignments/PayrollAccount.java
@@ -0,0 +1,43 @@
+class PayrollAccount {
+    private double basicSalary;
+    private double bonus;
+
+    public PayrollAccount(double openingSalary) {
+        if (openingSalary < 0) {
+            System.out.println("Warning: Opening basic salary cannot be negative. Initializing to 0.");
+            this.basicSalary = 0;
+        } else {
+            this.basicSalary = openingSalary;
+        }
+        this.bonus = 0;
+    }
+
+    public void creditBonus(double amount) {
+        if (amount <= 0) {
+            System.out.println("Error: Bonus amount must be greater than zero.");
+        } else {
+            this.bonus += amount;
+            System.out.println("Bonus credited: Rs " + amount);
+        }
+    }
+
+    public void deductTax(double percent) {
+        if (percent < 0 || percent > 100) {
+            System.out.println("Error: Tax percentage must be between 0 and 100.");
+        } else {
+            this.basicSalary -= this.basicSalary * (percent / 100);
+            System.out.println("Tax deducted: " + (int)percent + "%");
+        }
+    }
+
+    public double getNetSalary() {
+        return basicSalary + bonus;
+    }
+
+    public static void main(String[] args) {
+        PayrollAccount account = new PayrollAccount(50000);
+        account.creditBonus(5000);
+        account.deductTax(10);
+        System.out.println("Net salary: Rs " + account.getNetSalary());
+    }
+}
