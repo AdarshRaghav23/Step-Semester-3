160f6e15810cfca4d40ecde9f44c2f76561af4ad
AdarshRaghav23 <ar7087@srmist.edu.in>
2026-08-29 04:02:36 +0530

    Add files via upload

--- Assignments/Employee.java
+++ Assignments/Employee.java
@@ -1,30 +1,26 @@
 class Employee {
-    String empId;
     String empName;
     double salary;
-    boolean isIntern;
+    
+    static String companyName = "Bright Horizon Technologies";
+    static int employeeCount = 0;
 
-    public Employee(String empId, String empName, double salary) {
-        this.empId = empId;
+    public Employee(String empName, double salary) {
         this.empName = empName;
         this.salary = salary;
-        this.isIntern = false;
+        employeeCount++;
     }
 
-    public Employee(String empId, String empName) {
-        this(empId, empName, 0);
-        this.isIntern = true;
-    }
-
-    public void printProfile() {
-        System.out.println(empId + " | " + empName + " | Rs " + salary + " | Intern: " + isIntern);
+    public static void printCompanyInfo() {
+        System.out.println(companyName);
+        System.out.println("Employees on record: " + employeeCount);
     }
 
     public static void main(String[] args) {
-        Employee emp1 = new Employee("E-101", "Divya", 65000);
-        Employee emp2 = new Employee("E-102", "Arjun");
+        new Employee("Aarav", 50000);
+        new Employee("Bhavna", 60000);
+        new Employee("Chetan", 55000);
 
-        emp1.printProfile();
-        emp2.printProfile();
+        Employee.printCompanyInfo();
     }
 }
