3969ffe4d90df21a0f680d9648dae2233808205b
AdarshRaghav23 <ar7087@srmist.edu.in>
2026-08-29 04:07:23 +0530

    Add files via upload

---
+++ Practice Problems/Student.java
@@ -0,0 +1,25 @@
+class Student {
+    String name;
+    double attendance;
+
+    static String collegeName = "SRM Institute of Science and Technology";
+    static int studentCount = 0;
+
+    public Student(String name, double attendance) {
+        this.name = name;
+        this.attendance = attendance;
+        studentCount++;
+    }
+
+    public static void printCollegeInfo() {
+        System.out.println(collegeName);
+        System.out.println("Students created: " + studentCount);
+    }
+
+    public static void main(String[] args) {
+        new Student("Aditya", 85.5);
+        new Student("Priya", 92.0);
+
+        Student.printCollegeInfo();
+    }
+}
